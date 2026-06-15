9a6019ac5a6b8df24c881f0f29467a2f486a42df
Prabhat Yadav <prabhatyadav@Prabhats-MacBook-Pro.local>
2026-06-15 17:17:36 +0530

    Add production templates and assets

--- src/app.py
+++ src/app.py
@@ -18,7 +18,10 @@ import zipfile as zipf
 from flask import Flask, request, send_file, render_template, jsonify
 from src.fill_saving_calculations import run_fill
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="templates"
+)
 app.config["MAX_CONTENT_LENGTH"] = 500 * 1024 * 1024  # 500 MB
 
 BASE_DIR       = os.path.dirname(os.path.abspath(__file__))
